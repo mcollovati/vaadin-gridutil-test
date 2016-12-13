efdf39c537a0922a61b453e1cff04a2c681259c2
Marco Collovati <mcollovati@gmail.com>
2016-12-13 20:13:49 +0100

    first commit

---
+++ src/main/java/com/example/MyUI.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import com.vaadin.annotations.StyleSheet;
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.renderers.ClickableRenderer;
+import org.vaadin.gridutil.renderer.EditDeleteButtonValueRenderer;
+
+@Theme("valo")
+@SpringUI
+@StyleSheet({"style.css"})
+public class MyUI extends UI {
+  @Override
+  protected void init(VaadinRequest vaadinRequest) {
+
+    // Create a grid
+    Grid grid = new Grid();
+    grid.setSizeFull();
+    grid.setCaption("My Grid");
+
+    // Define some columns
+    grid.addColumn("id", Integer.class);
+    grid.addColumn("name", String.class);
+    grid.addColumn("born", Integer.class);
+
+    // Add some data rows
+    grid.addRow(1, "Nicolaus Copernicus", 1543);
+    grid.addRow(2, "Galileo Galilei", 1564);
+    grid.addRow(3, "Johannes Kepler", 1571);
+
+    grid.getColumn("id").setHeaderCaption("##");
+
+    //WITH THIS ENABLE WE HAVE A BLANK GRID
+    grid.getColumn("id").setRenderer(new EditDeleteButtonValueRenderer(new EditDeleteButtonValueRenderer.EditDeleteButtonClickListener() {
+    
+      @Override
+      public void onEdit(final ClickableRenderer.RendererClickEvent event) {
+        Notification.show(event.getItemId().toString() + " want's to get edited", Notification.Type.HUMANIZED_MESSAGE);
+      }
+    
+      @Override
+      public void onDelete(final com.vaadin.ui.renderers.ClickableRenderer.RendererClickEvent event) {
+        Notification.show(event.getItemId().toString() + " want's to get delete", Notification.Type.WARNING_MESSAGE);
+      }
+    }));
+
+    // first of all you need to set a custom style to the column
+    grid.setCellStyleGenerator(new Grid.CellStyleGenerator() {
+      @Override
+      public String getStyle(final Grid.CellReference cellReference) {
+        if (cellReference.getPropertyId()
+            .equals("id")) {
+          return "link-icon";
+        } else {
+          return null;
+        }
+      }
+    });
+
+    setContent(grid);
+  }
+}
